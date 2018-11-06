0f5377e08b0b1775f925466bb40d9bc7228cfbee
Marco Radavelli <marco.radavelli@unibg.it>
2018-11-06 09:00:58 +0100

    update springboot example

--- ExampleSpringBoot/src/main/webapp/hello/Application.java
+++ ExampleSpringBoot/src/main/webapp/hello/Application.java
@@ -5,16 +5,23 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(Application.class);
+    }
+    
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
